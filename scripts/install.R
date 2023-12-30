ca29d2eac1f1d9031e8b254904b4ce9dd933f328
Sebastian Kranz <sebkranz@gmail.com>
2023-12-30 19:50:20 +0100

    Initial commit

---
+++ scripts/install.R
@@ -0,0 +1,5 @@
+# Here we can add some custom package installations
+
+library(remotes)
+remotes::install_github("repboxr/sourcemodify", upgrade="never", force=TRUE)
+remotes::install_github("repboxr/repboxDB", upgrade="never", force=TRUE)
